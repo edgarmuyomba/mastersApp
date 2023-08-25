5661e33e88f7c830c535e7a08e773982cda30b98
edgarmuyomba <edgarmatthew247@gmail.com>
2023-08-25 21:18:56 +0300

    fixed feedback

--- lib/src/presentation/student/makeSubmission.dart
+++ lib/src/presentation/student/makeSubmission.dart
@@ -27,124 +27,120 @@ class _makeSubmissionState extends State<makeSubmission>
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: 
-      Container(
+      child: Container(
         height: 400,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(25))),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: TabBarView(controller: _tabController, children: [
-                      ListView(
-                        children: [
-                          Container(
-                            width: 250,
-                            child: TabBar(
-                                controller: _tabController,
-                                labelColor: Colors.green,
-                                unselectedLabelColor: Colors.grey,
-                                indicatorColor: Colors.green,
-                                tabs: [
-                                  Tab(
-                                      child: Text("Submissions",
-                                          textAlign: TextAlign.center)),
-                                  Tab(
-                                      child: Text(
-                                    "Feedback",
-                                    textAlign: TextAlign.center,
-                                  )),
-                                ]),
-                          ),
-                          SizedBox(height: 25.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                widget.task.title,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700, fontSize: 15),
-                              ),
-                              Card(
-                                color: Colors.green,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.file_upload_rounded,
-                                          color: Colors.white),
-                                      Text(
-                                        "Upload",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 16),
-                          Form(
-                              key: _formKey,
-                              child: Column(
-                                children: [
-                                  TextFormField(
-                                    controller: _comment,
-                                    maxLines: 5,
-                                    decoration: InputDecoration(
-                                        labelText: "Comment",
-                                        hintText: "Write a comment",
-                                        border: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Colors.black, width: 5.0),
-                                            borderRadius:
-                                                BorderRadius.circular(10))),
-                                    validator: (value) {
-                                      if (value!.isEmpty)
-                                        return "Please enter a comment";
-                                      return null;
-                                    },
-                                  ),
-                                  SizedBox(height: 10.0),
-                                  InkWell(
-                                      onTap: null,
-                                      child: Container(
-                                          alignment: Alignment.center,
-                                          width: double.infinity,
-                                          height: 40,
-                                          decoration: BoxDecoration(
-                                            color:
-                                                Color.fromARGB(255, 97, 233, 101),
-                                            borderRadius: BorderRadius.circular(10),
-                                          ),
-                                          child: Text(
-                                            "Submit",
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                                color:
-                                                    Color.fromARGB(255, 0, 72, 2),
-                                                fontWeight: FontWeight.w600,
-                                                fontSize: 15),
-                                          ))),
-                                  SizedBox(height: 10.0),
-                                ],
-                              ))
-                        ],
-                      ),
-                      ListView(
-                        children: [
-                          Container()
-                        ],
-                      )
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(25))),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                width: 250,
+                child: TabBar(
+                    controller: _tabController,
+                    labelColor: Colors.green,
+                    unselectedLabelColor: Colors.grey,
+                    indicatorColor: Colors.green,
+                    tabs: [
+                      Tab(
+                          child:
+                              Text("Submissions", textAlign: TextAlign.center)),
+                      Tab(
+                          child: Text(
+                        "Feedback",
+                        textAlign: TextAlign.center,
+                      )),
                     ]),
-                  ),
-                ],
               ),
-            ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+
+                  ListView(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.task.title,
+                          style:
+                              TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+                        ),
+                        Card(
+                          color: Colors.green,
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.file_upload_rounded,
+                                    color: Colors.white),
+                                Text(
+                                  "Upload",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: _comment,
+                              maxLines: 5,
+                              decoration: InputDecoration(
+                                  labelText: "Comment",
+                                  hintText: "Write a comment",
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.black, width: 5.0),
+                                      borderRadius: BorderRadius.circular(10))),
+                              validator: (value) {
+                                if (value!.isEmpty) return "Please enter a comment";
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 10.0),
+                            InkWell(
+                                onTap: null,
+                                child: Container(
+                                    alignment: Alignment.center,
+                                    width: double.infinity,
+                                    height: 40,
+                                    decoration: BoxDecoration(
+                                      color: Color.fromARGB(255, 97, 233, 101),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Text(
+                                      "Submit",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Color.fromARGB(255, 0, 72, 2),
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 15),
+                                    ))),
+                            SizedBox(height: 10.0),
+                          ],
+                        ))
+                  ]),
+                  ListView(
+                    children: [
+                      Container()
+                    ],
+                  )
+                  ]
+                ),
+              )
+            ],
           ),
+        ),
+      ),
     );
   }
 }
